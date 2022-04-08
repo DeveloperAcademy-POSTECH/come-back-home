db63c15a8dfdecd741aa106699ef761daa801f89
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-04-08 17:26:32 +0900

    feat: HomeView

---
+++ ComeBackHome/View/HomeView.swift
@@ -0,0 +1,44 @@
+//
+//  HomeView.swift
+//  ComeBackHome
+//
+//  Created by Jun.Mac on 2022/04/08.
+//
+
+import SwiftUI
+
+struct HomeView : View {
+
+    var body: some View {
+       
+        ZStack(){
+            
+            
+            
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            
+            VStack(){
+                
+                Image("lgloo")
+                
+            }
+            
+        }
+        
+        
+    }
+
+}
+
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+       
+        HomeView()
+        
+    }
+}
