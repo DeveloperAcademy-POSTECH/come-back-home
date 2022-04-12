2037b8396ffd0d327318a1f56920e132c1930468
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2022-04-12 14:16:05 +0900

    fix: UI, back button

---
+++ ComeBackHome/Struct/AppState.swift
@@ -0,0 +1,11 @@
+//
+//  AppState.swift
+//  ComeBackHome
+//
+//  Created by Jiyoung Park on 2022/04/12.
+//
+import Foundation
+
+final class AppState : ObservableObject {
+    @Published var menuRootViewId = UUID()
+}
