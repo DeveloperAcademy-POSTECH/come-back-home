ce0e31c8139b9ca2606d6e48c57c0984042d104c
Nam Geon Kim <namgeon1106@naver.com>
2022-04-12 15:59:38 +0900

    fix: LoginView 레이아웃 수정

---
+++ ComeBackHome/View/FirstTimeUse/StartingView.swift
@@ -0,0 +1,20 @@
+//
+//  StartingView.swift
+//  ComeBackHome
+//
+//  Created by 김남건 on 2022/04/12.
+//
+
+import SwiftUI
+
+struct StartingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartingView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartingView()
+    }
+}
